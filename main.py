8dc33d39f44294003fdf4b4dfb68c8555053f2f2
Laar3 <laar@tutanota.com>
2021-06-28 04:43:40 +0000

    working on support for windows. setting environment variables

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ results = myclient.search(input("Search: "), limit=10)
 tracks = results.get_tracks()   
 print("".join([f"{index + 1}: {track.name} - {track.artists[0].name} \n" for index, track in enumerate(tracks)]))
 download_url = tracks[int(input("Select: ")) - 1].url
-print(download_url)
+
+
+if os.name == 'posix':
+    os.system("export SPOTIPY_CLIENT_ID='bdfc939ee66f4c7b972a0bdd652bbbbb'")
+    os.system("export SPOTIPY_CLIENT_SECRET='cb054ef4f43f42a1955669e38826c51c'")
+elif os.name == 'nt':
+    os.system("set SPOTIPY_CLIENT_ID=bdfc939ee66f4c7b972a0bdd652bbbbb")
+    os.system("set SPOTIPY_CLIENT_SECRET=cb054ef4f43f42a1955669e38826c51c")
 
 command = f"spotify_dl -o {download_dir} -l {download_url}"
 os.system(command)
