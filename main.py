7e60b4bad0abbc88ace2cb39d9256d25ee646415
Laar3 <laar@tutanota.com>
2021-06-29 02:39:57 +0000

    now it will download the mp3 to the download directory rather than creating a directory in the download directory

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from spotifysearch.client import Client
 
 myclient = Client("bdfc939ee66f4c7b972a0bdd652bbbbb", "cb054ef4f43f42a1955669e38826c51c")
 results = myclient.search(input("Search: "), limit=10)
-tracks = results.get_tracks()
+tracks = results.get_tracks()   
 print("".join([f"{index + 1}: {track.name} - {track.artists[0].name} \n" for index, track in enumerate(tracks)]))
-download_url = tracks[int(input("Select: ")) - 1].url
-
+download_song = tracks[int(input("Select: ")) - 1]
+download_url = download_song.url
 
 if os.name == 'posix':
     print(" ")
 elif os.name == 'nt':
     print("unfortunately this program does not support windows yet, this is being worked on and will be fixed in the near future.")
 command = f"spotify_dl -o {download_dir} -l {download_url}"
+
 os.system(command)
+os.system(f"mv {download_dir}/{download_song.name}/*.mp3 {download_dir} ")
+os.system(f"rm -rfd {download_dir}/{download_song.name}")
