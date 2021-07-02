807edc2deeb9731b515f6b9ebda21f3f3708a0de
Laar3 <laar@tutanota.com>
2021-07-02 09:03:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import os
 sys.path.append(path.expanduser("~/.config/spotifysearch"))
 from config import *
 from spotifysearch.client import Client
+import time
 
 myclient = Client("bdfc939ee66f4c7b972a0bdd652bbbbb", "cb054ef4f43f42a1955669e38826c51c")
 results = myclient.search(input("Search: "), limit=10)
-tracks = results.get_tracks()   
+tracks = results.get_tracks()
 print("".join([f"{index + 1}: {track.name} - {track.artists[0].name} \n" for index, track in enumerate(tracks)]))
 download_song = tracks[int(input("Select: ")) - 1]
 download_url = download_song.url
@@ -16,8 +17,10 @@ if os.name == 'posix':
     print(" ")
 elif os.name == 'nt':
     print("unfortunately this program does not support windows yet, this is being worked on and will be fixed in the near future.")
-command = f"spotify_dl -o {download_dir} -l {download_url}"
+command = f"spotify_dl -o '{download_dir}' -l '{download_url}'"
 
 os.system(command)
-os.system(f"mv {download_dir}/{download_song.name}/*.mp3 {download_dir}")
-os.system(f"rm -rfd {download_dir}/{download_song.name}")
+
+os.system(f"rm '{download_dir}'/'{download_song.name}'/download_songs.txt")
+os.system(f"mv '{download_dir}'/'{download_song.name}'/* '{download_dir}'")
+os.system(f"rm -rfd '{download_dir}'/'{download_song.name}'")
